2f922f75c0f9621791a7ce0358edf0e05598712f
AnnaIon <159310102+AnnaIon@users.noreply.github.com>
2024-12-23 14:13:55 +0200

    build

--- rentApp/src/routes/Routes.jsx
+++ rentApp/src/routes/Routes.jsx
@@ -1,82 +1,77 @@
-import { createBrowserRouter,Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "../App";
 
-import Login from "../Pages/Login/Login"
-import Register from '../Pages/Register/Register';
-import Homepage from '../Pages/Homepage/Homepage';
-import MyProfile from '../Pages/MyProfile/MyProfile';
-import Favourites from '../Pages/Favourites/Favourites';
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import Homepage from "../Pages/Homepage/Homepage";
+import MyProfile from "../Pages/MyProfile/MyProfile";
+import Favourites from "../Pages/Favourites/Favourites";
 import MyFlats from "../Pages/MyFlats/MyFlats";
 import Auth from "../authentication/Auth";
 import AdminRoute from "./AdminRoute";
 import AllUsers from "../Pages/AllUsers/AllUsers";
 import Inbox from "../Pages/Inbox/Inbox";
 
-
-
-
 const routesArray = [
-    {
-		path: "/",
-		element: <App/>,
-		children: [
-			{
-				index: true,
-				element: <Navigate to="/authentication" />,
-			},
-			{
-				path: "authentication",
-				element: <Auth />,
-				children: [
-					{
-						index: true,
-						element: <Navigate to="login" />,
-					},
-					{
-						path: "login",
-						element: <Login />,
-					},
-					{
-						path: "register",
-						element: <Register />,
-					},
-				],
-			},
-			{
-				path: "homepage",
-				element: <Homepage />,
-		
-			},
-			{
-				path: "myflats",
-				element: <MyFlats />,
-			},
-			{
-				path: "myprofile",
-				element: <MyProfile />,
-			},
-			{
-				path: "favourites",
-				element: <Favourites />,
-			},
-			{
-				path: "inbox",
-				element: <Inbox />,
-			},
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/authentication" />,
+      },
+      {
+        path: "authentication",
+        element: <Auth />,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="login" />,
+          },
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "register",
+            element: <Register />,
+          },
+        ],
+      },
+      {
+        path: "homepage",
+        element: <Homepage />,
+      },
+      {
+        path: "myflats",
+        element: <MyFlats />,
+      },
+      {
+        path: "myprofile",
+        element: <MyProfile />,
+      },
+      {
+        path: "favourites",
+        element: <Favourites />,
+      },
+      {
+        path: "inbox",
+        element: <Inbox />,
+      },
 
-			{
-				path: "all-users",
-				element: <AdminRoute />,
-				children:[
-					{
-						path: "",
-						element: <AllUsers />,
-					},
-				]
-
-			},
-		],
-	},
+      {
+        path: "all-users",
+        element: <AdminRoute />,
+        children: [
+          {
+            path: "",
+            element: <AllUsers />,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 const Routes = createBrowserRouter(routesArray);
